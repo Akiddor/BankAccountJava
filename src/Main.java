eaefa04750ac391fce6a966e7326663cd50b3d2d
Brian Alexis Guerrero Alvídrez <brian.guerrero@crocsa.com.mx>
2025-03-27 12:01:37 -0600

    Add Busqueda Secuencial

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.desktop.SystemEventListener;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,10 +9,24 @@ public class Main {
         int i = 0, indice = 0;
         boolean encontrado = false;
 
+        while ((i< cuentas.length) && (!encontrado)) {
 
+            //Busqueda Secuencial
 
+            if(cuentas[i].getNumeroCuenta() == n) {
+                encontrado = true;
+                indice = i;
+            }
 
-        return n;
+            i++;
+
+        }
+
+        if (encontrado == false) {
+            indice = -1;
+        }
+
+        return indice;
     }
 
 
@@ -21,16 +36,16 @@ public class Main {
         String nombre;
         String apellido;
         String dni;
-        int numeroCuenta,nCuentas = 0,op;
-        double saldo;
+        int numeroCuenta,nCuentas = 0,op,indiceCuenta=0;
+        double saldo,cantidad;
         Cuenta cuentas[];
         Cliente cliente;
 
         // Bienvenida
-        System.out.printf("Bienvenido al banco Bañorte");
+        System.out.printf("\nBienvenido al banco Bañorte");
 
         //Verificacion de los datos del usuario (Cliente)
-        System.out.printf("Ingrese su nombre del usuario: ");
+        System.out.printf("\nIngrese su nombre del usuario: ");
         nombre = en.nextLine();
         System.out.printf("Ingrese su apellido del usuario: ");
         apellido = en.nextLine();
@@ -38,15 +53,15 @@ public class Main {
         dni = en.nextLine();
 
         // Pedimos el numero de cuentas que el usuario tiene a su disposicion
-        System.out.printf("Digite cuantas numero de cuentas tiene");
-        numeroCuenta = en.nextInt();
+        System.out.print("Digite cuantas numero de cuentas tiene: ");
+        nCuentas = en.nextInt();
         cuentas = new Cuenta[nCuentas];
 
         // Registrar el numero de cuentas que el usuario ingreso
         for (int i = 0; i < cuentas.length; i++) {
 
             System.out.println("\nDigite los datos para la cuenta " + (i + 1) + " : ");
-            System.out.println("Digite el numero de cuenta ");
+            System.out.println("\nDigite el numero de cuenta ");
             numeroCuenta = en.nextInt();
             System.out.println("Digite el saldo de la cuenta");
             saldo = en.nextDouble();
@@ -71,6 +86,56 @@ public class Main {
             switch (op) {
                 case 1:
                     System.out.println("\nDigite el numero de cuenta ");
+                    numeroCuenta = en.nextInt();
+                    indiceCuenta = buscarNumeroCuenta(cuentas,numeroCuenta);
+
+                    if (indiceCuenta == -1) {
+                        System.out.println("\nEl numero de cuenta ingresado no existe");
+                    }else {
+                        System.out.println("\nDigite la cantidad de dinero a ingresar:");
+                        cantidad = en.nextDouble();
+
+                        cliente.ingresar_dinero(indiceCuenta,cantidad);
+                        System.out.println("\nIngreso realizado correctamente");
+                        System.out.println("Saldo disponible:   " + cliente.consultar_saldo(indiceCuenta) );
+                    }
+                    break;
+
+                    case 2:
+                        System.out.println("\nDigite el numero de cuenta ");
+                        numeroCuenta = en.nextInt();
+                        indiceCuenta = buscarNumeroCuenta(cuentas,numeroCuenta);
+
+                        if (indiceCuenta == -1) {
+                            System.out.println("\nEl numero de cuenta ingresado no existe");
+                        }
+                        else {
+                            System.out.println("\nDigite la cantidad de dinero que desear retirar:");
+                            cantidad = en.nextDouble();
+                            if (cantidad > cliente.consultar_saldo(indiceCuenta)) {
+                                System.out.println("\nSaldo insuficiente");
+                            }else{
+                                cliente.retirar_dinero(indiceCuenta,cantidad);
+                                System.out.println("\nDinero retirado correctamente");
+                                System.out.println("Saldo disponible: " + cliente.consultar_saldo(indiceCuenta));
+                            }
+                        }
+                        break;
+                        case 3:
+                            System.out.println("\nDigite el numero de cuenta ");
+                            numeroCuenta = en.nextInt();
+                            indiceCuenta = buscarNumeroCuenta(cuentas,numeroCuenta);
+
+                            if (indiceCuenta == -1) {
+                                System.out.println("\nEl numero de cuenta ingresado no existe");
+                            }
+                            else {
+                                System.out.println("Saldo disponible: " + cliente.consultar_saldo(indiceCuenta));
+
+                            }
+                        case 4:
+                            break;
+                default: System.out.println("Opcion no valida");
 
             }
 
